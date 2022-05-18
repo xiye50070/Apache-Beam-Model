a91c0e3050bb74d89f2ad6a305b823f6e639decb
王铭章 <wangmingzhang@ysstech.com>
2022-05-18 18:18:51 +0800

    新增Apache beam 读取本地文件demo

---
+++ src/main/java/com/yss/beam/BantchWordCount.java
@@ -0,0 +1,49 @@
+package com.yss.beam;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class BantchWordCount {
+    public static void main(String[] args) {
+
+        PipelineOptions options = PipelineOptionsFactory.create();
+        Pipeline pipeline = Pipeline.create(options);
+
+        PCollection<KV<String, Long>> pCollection = pipeline
+//                .apply(TextIO.read().from("src/main/java/org/apache/beam/examples/yss/input/text.txt"))
+                .apply(Create.of("hello world","hello flink", "hello flink spark"))
+                .apply(FlatMapElements.into(TypeDescriptors.strings()).via(line -> Arrays.asList(line.split(" "))))
+                .apply(Count.perElement());
+
+        pCollection.apply(ParDo.of(new Println()));
+        pipeline.run();
+    }
+
+    // DoFn<a,b> a是输入类型，b是输出类型
+    static class Println extends DoFn<KV<String, Long>, String>{
+        /**
+         * processElement，过程元素处理方法，类似于spark、mr中的map操作
+         * 必须加上@ProcessElement竹节，并实现processElement方法
+         */
+        @ProcessElement
+        public void processElement(ProcessContext context){
+            // 从管道中取出的每个元素
+            KV<String, Long> element = context.element();
+            System.out.println(element);
+            if (element!=null){
+                context.output(element.toString());
+            }
+        }
+    }
+
+}
+
+
